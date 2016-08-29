d5a5f57b2de0c0be0f58c6827877fb6aa9c4b2d3
AJ NOURI <ajn@localhost.localdomain>
2016-08-29 04:30:41 +0200

    update

--- pkl/application.js
+++ pkl/application.js
@@ -1,27 +1,23 @@
-$('#random').on('click', function () {
-window.alert(theImages[whichImage]);
-var theImages = new Array() // do not change this
-// To add more image files, continue with the
-// pattern below, adding to the array.
+$('#random').click(function() {
 
-theImages[0] = 'https://h5p.org/h5p/embed/19684'
-theImages[1] = 'https://h5p.org/h5p/embed/21468'
-theImages[2] = 'https://h5p.org/h5p/embed/21469'
-theImages[3] = 'https://h5p.org/h5p/embed/21470'
+var sources = new Array()
 
+sources[0] = 'https://h5p.org/h5p/embed/19684'
+sources[1] = 'https://h5p.org/h5p/embed/21468'
+sources[2] = 'https://h5p.org/h5p/embed/21469'
+sources[3] = 'https://h5p.org/h5p/embed/21470'
 
-// do not edit anything below this line
 
 var j = 0
-var p = theImages.length;
+var p = sources.length;
 var preBuffer = new Array()
 
 for (i = 0; i < p; i++){
 	   preBuffer[i] = new Image()
-	   preBuffer[i].src = theImages[i]
+	   preBuffer[i].src = sources[i]
 		}
-	var whichImage = Math.round(Math.random()*(p-1));
-	  $('#question').attr('src', 'theImages[whichImage]');
+	var whichSource = Math.round(Math.random()*(p-1));
+	  $('#question').attr('src', 'sources[whichSource]');
     
 	  }
 
