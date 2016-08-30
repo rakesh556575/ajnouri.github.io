76c51356be3d1990e6c9d22e38af2836f677f56a
AJ NOURI <ajn@localhost.localdomain>
2016-08-30 03:32:03 +0200

    update

--- pkl/application.js
+++ pkl/application.js
@@ -1,23 +1,17 @@
-var sources = new Array()
+$(document).ready(function() {
+	var sources = new Array()
 
-sources[0] = 'https://h5p.org/h5p/embed/19684'
-sources[1] = 'https://h5p.org/h5p/embed/21468'
-sources[2] = 'https://h5p.org/h5p/embed/21469'
-sources[3] = 'https://h5p.org/h5p/embed/21470'
+	sources[0] = 'https://h5p.org/h5p/embed/19684'
+	sources[1] = 'https://h5p.org/h5p/embed/21468'
+	sources[2] = 'https://h5p.org/h5p/embed/21469'
+	sources[3] = 'https://h5p.org/h5p/embed/21470'
 
-var p = sources.length;
+	var p = sources.length;
 
-
-$(document).ready(function(){
-    var randomSource = Math.round(Math.random()*(p-1));
-    console.log(sources[randomSource]);
-    $('#question').attr('src', sources[randomSource]);
- 	$("#question").text("URL value is now: " + sources[randomSource]);        
-}
-
-$('#randomize').click(function() {
-    var randomSource = Math.round(Math.random()*(p-1));
-    console.log(sources[randomSource]);
-    $('#question').attr('src', sources[randomSource]);
- 	$("#question").text("URL value is now: " + sources[randomSource]);    
-}
+	$('#randomize').click(function() {
+	    var randomSource = Math.round(Math.random()*(p-1));
+	    console.log(sources[randomSource]);
+	    $('#question').attr('src', sources[randomSource]);
+	 	$("#question").text("URL value is now: " + sources[randomSource]);    
+    });
+});
